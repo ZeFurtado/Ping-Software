4cb9649e67db6fec645bc629afd90737c8dffc7c
Lukhas Daniel Korndorfer Furtado <lukhas.furtado@turing.ieduc.org.br>
2022-11-26 12:11:25 -0300

    Função príncipal funcionando e Laboratório 103 adicionado

---
+++ Ping-Software/Ping-Software/Computadores.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ping_Software
+{
+    public class Computadores
+    {
+        public List<string> lab103 = new List<string>();
+        List<string> lab104 = new List<string>();
+        List<string> lab106 = new List<string>();
+        List<string> lab107 = new List<string>();
+
+
+
+        public string[] Lab103()
+        {
+            lab103.Add("192.168.23.11");
+            lab103.Add("192.168.23.12");
+            lab103.Add("192.168.23.13");
+            lab103.Add("192.168.23.14");
+            lab103.Add("192.168.23.15");
+
+            return lab103.ToArray();
+        }
+
+    }
+
+}
--- Ping-Software/Ping-Software/Program.cs
+++ Ping-Software/Ping-Software/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.NetworkInformation;
+
+namespace Ping_Software
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Computadores labs = new Computadores();
+            labs.Lab103();
+           
+            Console.WriteLine("Em qual dos laboratório você gostaria de fazer Ping: ");
+            Console.WriteLine("1) 103   2) 104   3) 106   4) 107   ");
+            string opcao = Console.ReadLine();
+
+            switch (opcao) 
+            {
+                case "1":
+                    try
+                    {
+                        for (int i = 0; i <= labs.lab103.Count - 1; i++)
+                        {
+                            Ping ping = new Ping();
+                            PingReply resposta = ping.Send(labs.lab103[i], 1000);
+                            if (resposta.Status == IPStatus.Success)
+                            {
+                                Console.WriteLine("O computador está ligado!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("O computador está desligado");
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+            }
+
+        }
+    }
+}
